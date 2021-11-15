fece4894610b2428a6dece8e91ef5c3d22258645
RizwanMushtaq <rizwanmushtaq15@gmail.com>
2021-11-15 08:05:30 +0100

    makeitbetter

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,2 @@
 "use strict";
-var world = 'world';
-function hello(world) {
-    console.log("Hellooooooooo", world);
-}
-hello(world);
 //# sourceMappingURL=index.js.map
---
+++ dist/index1.js
@@ -0,0 +1,7 @@
+"use strict";
+const world = 'world';
+function hello(world) {
+    console.log("Hellooooooooo", world);
+}
+hello(world);
+//# sourceMappingURL=index1.js.map
